6c0a69b544ecd76322f10a0278b7271dadd0417f
Tensor-DeepLearning <Tensor-DeepLearning@users.noreply.github.com>
2017-07-15 10:02:16 +0800

    Delete tensorTubeTranspose.m

--- tensorTubeTranspose.m
+++
@@ -1,9 +0,0 @@
-function [T_t] = tensorTubeTranspose(T)
-%% 对tensor的每一个前切面矩阵做转置
-    szT = size(T);
-    T_t = zeros(szT(2), szT(1), szT(3));
-    
-    for l = 1 : szT(3)
-        T_t(:, :, l) = T( :, :, l)';
-    end
-end
